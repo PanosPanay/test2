e7b10f17af4a57123a280a6610cab26ec523ac07
PanosPanay <edwinwangbj@outlook.com>
2022-05-17 11:21:07 +0800

    hot-fix branch commit.

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main()
     cout << "hello world!" << endl;
     cout << "third commit!" << endl;
     cout << "4th commit!" << endl;
+    cout << "hello hot-fix branch!" << endl;
     return 0;
 }
